ddd70de1b66de2541d6d99b34f676d5072163c78
Sherif <sherif@gjirafa.com>
2024-01-23 15:14:03 +0100

    Added testing for weatherbox component

--- components/weatherBox/weatherBox.test.tsx
+++ components/weatherBox/weatherBox.test.tsx
@@ -1,31 +1,41 @@
-// WeatherBox.test.ts
-import { render, screen } from "@testing-library/react";
-import { useSelector } from "react-redux";
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { makeStore, AppStore } from "@/redux/store";
 import WeatherBox from "./weatherBox";
+import TempInfo from "./tempInfo";
+import { IWeather } from "@/interfaces/IWeather";
+import MainInfo from "./mainInfo";
 
-jest.mock("react-redux");
-
-describe("WeatherBox component", () => {
-  test("renders WeatherBox component with appropriate styles", () => {
-    // Manually cast useSelector to a mock function
-    const useSelectorMock = useSelector as any;
-
-    // Assuming your state structure, modify the mockReturnValue accordingly
-    useSelectorMock.mockReturnValue({
-      weather: {
-        value: {
-          name: "Test City",
-          sys: { country: "Test Country" },
-          weather: [{ icon: "test-icon" }],
-          dt: 1642981200,
-          timezone: 7200,
-          main: { temp: 2 },
-        },
-      },
-    });
-
-    render(<WeatherBox />);
-
-    // ... rest of the test
+describe("Weatherbox component with provider", () => {
+  test("renders correctly with mocking data", () => {
+    const store = makeStore();
+    const { getByTestId, getByRole } = render(
+      <Provider store={store}>
+        <WeatherBox />
+      </Provider>
+    );
+    const weatherBoxElement = getByTestId("weather-box");
+    const heading1 = getByRole("heading", { level: 1 });
+    expect(weatherBoxElement).toHaveClass("weatherbox");
+    expect(weatherBoxElement).toHaveClass("glass");
+    expect(heading1).toBeInTheDocument();
+  });
+  test("temp component", () => {
+    const { main, weather } = {
+      main: {} as IWeather["main"],
+      weather: {} as IWeather["weather"],
+    };
+    const { getByTestId } = render(<TempInfo main={main} weather={weather} />);
+    const tempEl = getByTestId("main-temp");
+    const titleEl = getByTestId("main-title");
+    expect(tempEl).toBeInTheDocument();
+    expect(titleEl).toBeInTheDocument();
+  });
+  test("main component info render correctly and it contains three list items", () => {
+    const mainInfo = {} as IWeather["main"];
+    const { getByRole, getAllByRole } = render(<MainInfo {...mainInfo} />);
+    expect(getByRole("list")).toBeInTheDocument();
+    expect(getAllByRole("listitem")).toHaveLength(3);
   });
 });
